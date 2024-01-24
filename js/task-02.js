91ca5aaae8a12d41c037eca64e79cd04fb4ccb1d
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-01-24 14:52:13 -0800

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+
+ingredients.forEach((ingredient) => {
+  const liELement = document.createElement('li');
+  liELement.textContent = ingredient;
+  liELement.classList.add('item');
+  ingredientsList.appendChild(liELement);
+});
