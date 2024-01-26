aff64742822acfd8ed5b980fba156cda3de44ec8
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-01-26 08:07:51 -0800

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+document.querySelector('.login-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const emailInput = this.elements.email;
+    const passwordInput = this.elements.password;
+
+    if (emailInput.value === '' || passwordInput.value === '') {
+        alert('All fields must be filled in');
+        return;
+    }
+    const formData = {
+        email: emailInput.value,
+        password: passwordInput.value
+    };
+    console.log(formData);
+    this.reset();
+});
 
