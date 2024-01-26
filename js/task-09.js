be9eb7448c65208c5c21bcb8315f5322e53190b9
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-01-26 08:22:37 -0800

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyElement = document.body;
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+changeColorButton.addEventListener('click', function () {
+  const randomColor = getRandomHexColor();
+  bodyElement.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
